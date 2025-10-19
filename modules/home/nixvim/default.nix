65c60138ca399613b2dd98807689d96c65b21176
Tony Du <tonydu121@hotmail.com>
2025-10-18 20:20:34 -0700

    zls

--- modules/home/nixvim/default.nix
+++ modules/home/nixvim/default.nix
@@ -230,20 +230,20 @@ in
               "biome"
               "biome-check"
               "biome-organize-imports"
-              "eslint_d"
+              # "eslint_d"
             ];
             typescript = [
               "biome"
               "biome-check"
               "biome-organize-imports"
-              "eslint_d"
+              # "eslint_d"
             ];
             haskell = [ "stylish-haskell" ];
             nix = [ "nixpkgs_fmt" ];
             bash = [ "shellcheck" ];
           };
           default_format_opts = {
-            lsp_format = "never";
+            lsp_format = "fallback";
           };
         };
       };
@@ -360,9 +360,12 @@ in
 
           ts_ls.enable = true;
           # biome.enable = true;
-          denols = {
-            enable = false;
-            package = null;
+          # denols = {
+          #   enable = true;
+          #   package = null;
+          # };
+          zls = {
+            enable = true;
           };
 
           # weird guys
